6c34a4318404cf5a8e25b5d7fee59cb666b680ef
athulyaliving1 <110807506+athulyaliving1@users.noreply.github.com>
2024-04-25 12:49:56 +0530

    Update Url.jsx

--- src/utilities/Url.jsx
+++ src/utilities/Url.jsx
@@ -2,5 +2,10 @@
 //   ("Access-Control-Allow-Origin", "http://localhost:4040");
 // export const URLDevelopment =
 //   ("Access-Control-Allow-Origin", "http://164.52.220.10:4040");
+// export const URLDevelopment =
+//   ("Access-Control-Allow-Origin", "http://124.123.68.117:4040");
 export const URLDevelopment =
-  ("Access-Control-Allow-Origin", "http://124.123.68.117:4040");
+  ("Access-Control-Allow-Origin", "https://aal.athulyaliving.net");
+
+
+
